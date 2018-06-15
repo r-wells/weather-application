0e07044ddbb2fc81d4c7ad15741306d967a763af
Ryan <r.l.wells@outlook.com>
2018-06-15 07:59:42 -0700

    running eject

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+import Cloudy from '../assets/images/cloudy.PNG';
+
 class Weather extends Component {
     render () {
 
     return (
     <div className="WeatherDiv">
         <p>This is the weather app</p>
+        <img src={Cloudy} alt="Cloudy Image" />
     </div>
     );
 }
