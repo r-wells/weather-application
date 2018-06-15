ee8b0c63f91e26e1c2258b87594183a4a9b15f46
Ryan <r.l.wells@outlook.com>
2018-06-15 08:14:51 -0700

    added CSS modules

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import Weather from './components/Weather';
 import logo from './logo.svg';
-import './App.css';
+import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to My React App</h1>
+      <div className={classes.App}>
+        <header className={classes.AppHeader}>
+          <img src={logo} className={classes.AppLogo} alt="logo" />
+          <h1 className={classes.AppTitle}>Welcome to My React App</h1>
         </header>
-        <p className="App-intro">
+        <p className={classes.AppIntro}>
           This is a weather application that displays the weather for a given region.
         </p>
           <Weather />
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
+import classes from './Weather.css';
 import Cloudy from '../assets/images/cloudy.PNG';
 
 class Weather extends Component {
     render () {
 
     return (
-    <div className="WeatherDiv">
+    <div className={classes.WeatherDiv}>
         <p>This is the weather app</p>
         <img src={Cloudy} alt="Cloudy Image" />
     </div>
