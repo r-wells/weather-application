c6738ebcf7968e3e4ffdfd7e84cfa3115a6af998
Ryan <r.l.wells@outlook.com>
2018-06-18 10:58:18 -0700

    added dates via API call

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,26 +6,17 @@ import classes from './Weather.css';
 
 class Weather extends Component {
 
-    componentDidMount () {
-        axios.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=77b11a8fa7b1f45be18d23910a441f75')
-            .then(response => {
-                console.log(response);
-            });
-
-    }
-    
     state = {
-        forecast: [
-          { id: 'qwkdik', date: 'Monday', temp: 90},
-          { id: 'ponu', date: 'Tuesday', temp: 90},
-          { id: 'dncscs', date: 'Wenesday', temp: 90},
-          { id: 'dnoc', date: 'Thursday', temp: 90},
-          { id: 'pioun', date: 'Friday', temp: 90},
-          { id: 'adwwneu', date: 'Saturday', temp: 90},
-          { id: 'cueih', date: 'Sunday', temp: 90}
-        ],
-        showBottom: false
+        forecasts: [{}]
       }
+
+    componentDidMount () {
+        axios.get('http://api.openweathermap.org/data/2.5/forecast?q=London,uk&appid=77b11a8fa7b1f45be18d23910a441f75')
+            .then(response => {
+                this.setState({forecasts: response.data.list});
+                console.log(this.state.forecasts);
+            });
+    }
     
       displayBottomHandler = () => {
         const doesShow = this.state.showBottom;
@@ -34,24 +25,29 @@ class Weather extends Component {
 
     render() {
 
-    return (
-        <div className={classes.WeatherDiv}>
-            {this.state.forecast.map((day, index) => {
-                return <Subweather
-                click={() => this.displayBottomHandler(index)} 
-                date={day.date}
-                temp={day.temp}
-                key={day.id} />
-            })}
-            {/* {
-                this.state.showBottom === true ?
-                <div>
-                    <p>Displays an hourly forecast when clicked</p>
-                </div>: null
-            } */}
-        </div>
-    );
-}
+        const projections = this.state.forecasts.map(forecast => {
+            return <Subweather
+                    date={forecast.dt_txt}
+                    temp={forecast.temp}
+                    key={forecast.id}  /> ;
+        });
+
+        return (
+            <div className={classes.WeatherDiv}>
+                {projections[0]}
+                {projections[8]}
+                {projections[16]}
+                {projections[24]}
+                {projections[32]}
+                {/* { {
+                    this.state.showBottom === true ?
+                    <div>
+                        <p>Displays an hourly forecast when clicked</p>
+                    </div>: null
+                } } */}
+            </div>
+        );
+    }
 }
 
 export default Weather;
